b6bccf6471509a2b4e7b7d2f65c042deb7c2c4f0
Blockaroz <lbpk.123fan@gmail.com>
2020-08-09 20:20:15 -0400

    Revert "Revert "mod changes""
    This reverts commit 9fc37c1f775c689cbd780f30fb842fe301f7ce16.

---
+++ Dusts/TropidiumSteam.cs
@@ -0,0 +1,37 @@
+﻿using DarknessUnbound.Helpers;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Dusts
+{
+    public class TropidiumSteam : ModDust
+    {
+        public override void OnSpawn(Dust dust)
+        {
+            dust.color = default;
+            dust.scale *= Main.rand.NextFloat(1.2f, 2f);
+            dust.alpha = 175;
+        }
+
+        public override bool Update(Dust dust)
+        {
+            dust.rotation += dust.velocity.X * 0.2f;
+            dust.velocity *= 1.09f;
+            dust.scale *= 0.975f;
+            Color red = new Color(99, 10, 50);
+            Color blue = new Color(7, 43, 96);
+            Lighting.AddLight(dust.position, new AnimatedColor(red, blue).LightingColor());
+            if (dust.scale < 0.05f)
+            {
+                dust.active = false;
+            }
+            return true;
+        }
+        public override Color? GetAlpha(Dust dust, Color lightColor)
+        {
+            return new Color(255, 255, 255, dust.alpha);
+        }
+    }
+}
---
+++ Items/Tropidium/SteamCannon.cs
@@ -0,0 +1,53 @@
+﻿using DarknessUnbound.Projectiles.Tropidium;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items.Tropidium
+{
+	public class SteamCannon : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Steam Cannon");
+			Tooltip.SetDefault("Blasts hot steam a short distance ahead");
+			Item.staff[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 25;
+			item.magic = true;
+			item.mana = 6;
+			item.width = 44;
+			item.height = 46;
+			item.useTime = 7;
+			item.useAnimation = 7;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.value = 32500;
+			item.rare = ItemRarityID.Blue;
+			item.UseSound = SoundID.Item34;
+			item.autoReuse = true;
+			item.channel = true;
+			item.shoot = ModContent.ProjectileType<SteamBubble>();
+			item.shootSpeed = 25f;
+			item.noMelee = true;
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+			Vector2 posDiff = position += new Vector2(Main.rand.Next(-5, 5));
+			Projectile.NewProjectile(posDiff.X, posDiff.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+			return false;
+        }
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			//recipe.AddIngredient(insert bar);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
