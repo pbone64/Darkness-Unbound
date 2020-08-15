077c4d4624ffce58db81d6d4dbf8cb489a8a3f06
Pbone3 <captainfluffypants0@gmail.com>
2020-08-15 12:04:09 -0500

    Ethos of terraria progress

---
+++ NPCs/Bosses/EthosOfTerraria/CopypastaManager.cs
@@ -0,0 +1,44 @@
+﻿namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
+{
+    public static class CopypastaManager
+    {
+        public static string[] Potato_One() => new string[] {
+                "What is wrong with you? Why do you insist on being such a piece of garbage not doing anythin g while still demanding praise from your shitty ECHPRINGLEBEGONINGMONKAS lackeys",
+                "Your entire Ant-calameme - ech - tron movement is creating a huge tumor, instilling the false idea that every mod must bow down to you",
+                "Can you just leave the terraria community? It has been a displeasuring experience having to deal with literally every single bug and hindering movement in the community coming from this discord",
+                "Alright cool. I'm sure those murder victims you used the 'blame the victim' idea on will very much appreciate seeing your little religion torn down"
+            };
+
+        public static string[] Potato_Two() => new string[] {
+            "Earth Lord behaves similarly like Moon Lord, but more challenging, powerful, and faster.",
+            "He has 2 hands, 5 eyes, a mouth, 2 portals above his hands, and a heart (core).",
+            "Like Moon Lord's background, the background on screen is a dark blue tint when Earth Lord is alive.",
+            "Earth Lord appears to be more massive and gargantuan than Moon Lord, which over fits the entire screen.",
+            "Like Moon Lord, He has multiple attacks to eradicate the player. He has way more health than Moon Lord, serving over 8 million / 12 million  in total.",
+            "Earth Lord also has 4 additional attacks compared to Moon Lord. He is a very defensive creature, with 785 / 1370 defense in total, making one of the opponents with very high defense.",
+            "Earth Lord is also a farmable creature. Earth Lord appears to be a cheat-free boss, because of how many attacks, defense, strength, HP, amount, and different stuff.",
+            "Earth Lord has 2 extra eyes on his head, instead of one. Earth Lord now have 2 portals above his hands that summon one of his own respective minions. Earth Lord's attacks are primarily enormous waves or floods of projectiles, but most of the attacks deal very high damage. Earth Lord looks almost pretty impossible to defeat, because of the many attacks, strength, speed, Al, power, difficulty, amount, the number of minions, and so on. Earth Lord's attack pattern style is similar to Moon Lord's attack pattern. Due to how many attacks, the high HP, high-dmg attacks, strength, speed, Al, amount, power, and difficulty, Earth Lord is the superboss. Earth Lord has the same death animation and sound as Moon Lord when he is completely defeated."
+        };
+
+        public static string[] MUTATED_FROM_FARGO_SOULS() => new string[] {
+            "YOU ARE NOTHING COMPARED TO THE ERODED SPIRITS!",
+            "I BRING FORTH THE END UPON THE FOOLISH, THE UNWORTHY!",
+            "YOU WANT TO DEFEAT ME? MAYBE IN TWO ETERNITIES!",
+            "DIE, FOOLISH TERRARIAN!",
+            "THEY SAID THERE WAS 3 END BRINGERS, BUT I AM THE FOURTH, A BREAKER OF REALITY!",
+            "HELL DOESN’T ACCEPT SCUM LIKE YOU, SO SUFFER FOREVER IN MY ENDLESS ONSLAUGHT OF INFINITE POWER, THE POTENTIAL OF ETERNITIES STRETCHED TO THE ABSOLUTE MAXIMUM APOTHEOSES!",
+            "YOUR UNHOLY SOUL SHALL BE CONSUMED BY DEPTHS LOWER THAN THE DEEPEST REACHES OF HELL!",
+            "I CONTROL THE POWER THAT HAS REACHED FROM THE FAR ENDS OF THE UNIVERSE, UNITING DIMENSIONS, MANIPULATING TERRARIA, SLAYING MASOCHIST, AND JUDGING HEAVENS!",
+            "FOR CENTURIES I HAVE TRAINED FOR ONE GOAL ONLY: PURGE THE WORLD OF THE UNWORTHY, SLAY THE WEAK, AND BRING FORTH TRUE POWER.",
+            "IN THE HIGHEST REACHES OF HEAVEN, MY BROTHER RULES OVER THE SKY! SOON, ALL OF TERRARIA WILL BE PURGED OF THE UNWORTH AND A NEW AGE WILL START!",
+            "A NEW AGE OF AWESOME! A GOLDEN AGE WHERE ONLY ABSOLUTE BEINGS EXIST! DEATH, INFERNO, TIDE; I AM THE OMEGA AND THE ALPAA, THE BEGINNING AND THE END! ALMIGHTY POWER; REVELATIONS.",
+            "ABSOLUTE BEING, ABSOLUTE PURITY. WITHIN THE FOOLISH BANTERINGS OF THE MORTAL WORLD I HAVE ACHIEVED POWER THAT WAS ONCE BANISHED TO THE EDGE OF THE GALAXY! I BRING FOR CALAMITIES, CATASTROPHES, AND CATACLYSM;",
+            "ELDRITCH POWERS DERIVED FROM THE ABSOLUTE WORD OF FATE.",
+            "FEEL MY UBIQUITOUS WRATH DRIVE YOU INTO THE GROUND AS A WORLD SHAPER DRIVES HIS WORLD INTO REALITY!",
+            "THE SHARPSHOOTER’S EYE PALES IN COMPARISON OF MY PERCEPTION OF REALITY! BERSERKERS RAGE NAUGHT BUT A BUNNIES!",
+            "OLYMPIANS A MINOR GOD, ARCH WIZARDS A POSER! A MASTERY OF FLIGHT, THE IRON WILL OF A COLOSSUS; BOTH ELEMENTARY CONCEPTS!",
+            "A CONJUROR BUT A PEDDLING MAGICIAN, A TRAWLER A SLIVER COMPARED TO MY LIFE MASTERY? SUPERSONIC SPEED, LIGHTSPEED TIME!",
+            "GLORIOUS LIGHT SHALL ELIMINATE YOU, YOU FOOLISH BUFFOON!"
+        };
+    }
+}
--- NPCs/Bosses/EthosOfTerraria/TerrariaEthos.cs
+++ NPCs/Bosses/EthosOfTerraria/TerrariaEthos.cs
@@ -2,7 +2,10 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
 using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
@@ -14,6 +17,7 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
     {
         public static Texture2D CultistRingTexture;
         public static Texture2D BlurTexture;
+        public const bool FUNNY_BOSS = false;
 
         public override void SetStaticDefaults()
         {
@@ -58,10 +62,15 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
         private bool drawRing = true;
         private bool dialogue = true;
         private bool initialized = false;
+        private bool superAttacks = false;
         private float ringRotation = 0;
         private int oldDialogueQuotient = 0;
         private int dialogueQuotient = 0;
         AttackProfile attack;
+        private int nextCopypasta = 0;
+        private int saidCopypasta = 0;
+        private int copypastaCounter = 0;
+        private int saidHpPercentages = 0;
 
         public float DialogueTimer { get => npc.ai[0]; set => npc.ai[0] = value; }
         public float DialogueState { get => npc.ai[1]; set => npc.ai[1] = value; }
@@ -71,6 +80,11 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
         public float InAttackTimer1 { get => npc.localAI[0]; set => npc.localAI[0] = value; }
         public float InAttackTimer2 { get => npc.localAI[1]; set => npc.localAI[1] = value; }
         public float AttackFinished { get => npc.localAI[2]; set => npc.localAI[2] = value; }
+        public float PassiveDialogueTimer { get => npc.localAI[3]; set => npc.localAI[3] = value; }
+
+        private float PassiveDialogueState;
+        private int oldPassiveDialogueQuotient = 0;
+        private int passiveDialogueQuotient = 0;
 
         public override void AI()
         {
@@ -150,15 +164,14 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
                             DialogueState++;
                             DialogueTimer = 0;
                             dialogue = false;
+                            npc.dontTakeDamage = false;
+                            npc.immortal = false;
                             break;
                     }
                 }
             }
             else
-            {
-                npc.dontTakeDamage = false;
-                npc.immortal = false;
-            }
+                PassiveDialogue();
             // END OF DIALOGUE
 
             if (!dialogue)
@@ -211,13 +224,327 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
         {
             InAttackTimer1++;
 
-            Main.NewText("RANDOM BULLETS");
+            //Main.NewText("RANDOM BULLETS");
 
             if (InAttackTimer1 >= 90)
             {
                 AttackFinished = 1f;
             }
         }
+
+        #region DIALOGUE
+        private void PassiveDialogue()
+        {
+            PassiveDialogueTimer++;
+            oldPassiveDialogueQuotient = passiveDialogueQuotient;
+            passiveDialogueQuotient = (int)(PassiveDialogueTimer / 95);
+
+            if (oldPassiveDialogueQuotient != passiveDialogueQuotient)
+            {
+                switch (passiveDialogueQuotient)
+                {
+                    case 1:
+                        switch (PassiveDialogueState)
+                        {
+                            case 0:
+                                chat("It was a beautiful day outside, until you came along...");
+                                break;
+                            case 1:
+                                chat("Everything points towards massive anomalies in the spacetime continuum. Worlds starting, worlds ending...");
+                                break;
+                            case 2:
+                                chat("To be honest, it just makes it harder for me to give it my all...");
+                                break;
+                            case 3:
+                                chat("When you first appeared here, I imagined us fighting by eachother's side...");
+                                break;
+                            case 4:
+                                chat("After seeing you butcher them whole, I can't help but get this feeling...");
+                                break;
+                            case 5:
+                                chat("A message to the master of this world, if I should die before this blongus does...");
+                                break;
+                            case 6:
+                                chat("I can try my best...");
+                                break;
+                            case 7:
+                                chat("Auegh..! Now you've done it!");
+                                superAttacks = true;
+                                break;
+                            case 8:
+                                chat("CONVULSE ON THE FLOOR UNTIL THE SIRENS GO OFF", true);
+                                goto end;
+                            case 9:
+                                chat("You think you're invincable...", sad: true);
+                                break;
+                            case 10:
+                                chat("You were known as a threat the moment you killed the eye...");
+                                break;
+                            case 11:
+                                chat("There's no way...!");
+                                break;
+                            case 12:
+                                
+                                break;
+                        }
+                        break;
+                    // END OF CASE
+
+                    case 2:
+                        switch (PassiveDialogueState)
+                        {
+                            case 0:
+                                chat("You've already been beaten down by those you've killed...");
+                                break;
+                            case 1:
+                                chat("Every time you die, timelines jump...");
+                                break;
+                            case 2:
+                                chat("Knowing that no matter what, you'll always win...");
+                                break;
+                            case 3:
+                                chat("Fending off the corruption, protecting the hallowed...");
+                                break;
+                            case 4:
+                                chat("I can't afford to let you live any longer", sad: true);
+                                goto end;
+                            case 5:
+                                chat("They will dimolish whatever we make. They dare stand in your way", sad: true);
+                                goto end;
+                            case 6:
+                                chat("But if I die before I continue...");
+                                break;
+                            case 7:
+                                chat("YOU'VE SCALED IN POWER TOO MUCH! NO MORE HOLDING BACK", true);
+                                goto end;
+                            case 9:
+                                chat("But I can see through your hollow lies.", sad: true);
+                                goto end;
+                            case 10:
+                                chat("Your wrath, but a sliver compared to mine.....");
+                                break;
+                            case 11:
+                                chat("Should you win... my final hour willl tick", sad: true);
+                                goto end;
+                        }
+                        break;
+                    // END OF CASE
+
+                    case 3:
+                        switch (PassiveDialogueState)
+                        {
+                            case 0:
+                                chat("I'M JUST FINISHING THE JOB", true);
+                                goto end;
+                            case 1:
+                                chat("It's almost like...");
+                                break;
+                            case 2:
+                                chat("No matter who should win, no matter who's in the right...");
+                                break;
+                            case 3:
+                                chat("But no.", sad: true);
+                                break;
+                            case 6:
+                                chat("Well, let's just say...");
+                                break;
+                            case 10:
+                                chat("I have been sent to dimolish you upon the Master's demand...", true);
+                                goto end;
+                        }
+                        break;
+                    // END OF CASE
+
+                    case 4:
+                        switch (PassiveDialogueState)
+                        {
+                            case 1:
+                                chat("YOU'RE A DIRTY CHEATER", true);
+                                goto end;
+                            case 2:
+                                chat("Heh, hell if I know if that's just an excuse to be lazy", sad: true);
+                                goto end;
+                            case 3:
+                                chat("You'll keep coming back, attempting to end my life.", sad: true);
+                                goto end;
+                            case 6:
+                                chat("Victory will stir in their favour");
+                                goto end;
+                        }
+                        break;
+                    // END OF CASE
+
+                    end:
+                        PassiveDialogueState++;
+                        PassiveDialogueTimer = 0;
+                        break;
+                }
+            }
+
+            if (FUNNY_BOSS) checkCopypasta();
+            hpPercentageDialogue();
+        }
+
+        private void checkCopypasta()
+        {
+            nextCopypasta = 0;
+
+            if (npc.life < npc.lifeMax / 3f * 2)
+            {
+                nextCopypasta++;
+            }
+
+            if (npc.life < npc.lifeMax / 2f)
+            {
+                nextCopypasta++;
+            }
+
+            if (npc.life < npc.lifeMax / 3f)
+            {
+                nextCopypasta++;
+            }
+
+            if (saidCopypasta != nextCopypasta)
+            {
+                List<string> cp = new List<string>();
+                switch (nextCopypasta)
+                {
+                    case 1:
+                        cp = CopypastaManager.Potato_One().ToList();
+                        break;
+                    case 2:
+                        cp = CopypastaManager.Potato_Two().ToList();
+                        break;
+                    case 3:
+                        cp = CopypastaManager.MUTATED_FROM_FARGO_SOULS().ToList();
+                        break;
+                }
+
+                copypasta(cp.ToArray(), nextCopypasta);
+            }
+        }
+
+        private void hpPercentageDialogue()
+        {
+            string text = "";
+
+            if (npc.life < npc.lifeMax / 10f * 9f && saidHpPercentages < 1)
+            {
+                text = "Have you taken the right path?";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 8.75f && saidHpPercentages < 2)
+            {
+                text = "Are your morals mistaken?";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 8.5f && saidHpPercentages < 3)
+            {
+                text = "You, who could save us all...";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 8.35f && saidHpPercentages < 4)
+            {
+                text = "All I have sacrificed...";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 8.45f && saidHpPercentages < 5)
+            {
+                text = "Petty emoitions control your soul...";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 8f && saidHpPercentages < 6)
+            {
+                text = "Who shall survive your wrath?";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 7.75f && saidHpPercentages < 7)
+            {
+                text = "Auegh...! You've grown too strong!";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 7.25f && saidHpPercentages < 8)
+            {
+                text = "I can't hold out for must longer...";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 7f && saidHpPercentages < 9)
+            {
+                text = "Soon, It'll all be consumed...";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 6.75f && saidHpPercentages < 10)
+            {
+                text = "Give up! It'll make this less painful for everyone!";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 6.5f && saidHpPercentages < 11)
+            {
+                text = "Alright, now I'M mad!!!!";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 6f && saidHpPercentages < 12)
+            {
+                text = "Can we just stop this?!";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 5.75f && saidHpPercentages < 13)
+            {
+                text = "Niether of us will die";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 5.5f && saidHpPercentages < 14)
+            {
+                text = "Time will keepy... *huff* rewinding *huff*";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 5.25f && saidHpPercentages < 15)
+            {
+                text = "Eurgh..! We'll never die!";
+                saidHpPercentages++;
+            }
+
+            if (npc.life <= npc.lifeMax / 10f * 5f && saidHpPercentages < 16)
+            {
+                text = "I guess I could just... become invincible...";
+                npc.dontTakeDamage = true;
+                npc.immortal = true;
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 5f && saidHpPercentages < 17 && Main.GameUpdateCount % 110 == 0)
+            {
+                text = "Then you'll never kill me";
+                saidHpPercentages++;
+            }
+
+            if (npc.life < npc.lifeMax / 10f * 5f && saidHpPercentages < 18 && Main.GameUpdateCount % 110 == 0)
+            {
+                text = "Will you spare me..? Or finish the job?";
+                saidHpPercentages++;
+            }
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                chat(text);
+                CombatText.NewText(npc.getRect(), Color.Lime, text, true);
+            }
+        }
+        #endregion
 
         public override bool PreDraw(SpriteBatch spriteBatch, Color drawColor)
         {
@@ -246,6 +573,8 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
             writer.Write(npc.localAI[0]);
             writer.Write(npc.localAI[1]);
             writer.Write(npc.localAI[2]);
+            writer.Write(npc.localAI[3]);
+            writer.Write(PassiveDialogueState);
         }
 
         public override void ReceiveExtraAI(BinaryReader reader)
@@ -257,14 +586,35 @@ namespace DarknessUnbound.NPCs.Bosses.EthosOfTerraria
             npc.localAI[0] = (float)reader.ReadDouble();
             npc.localAI[1] = (float)reader.ReadDouble();
             npc.localAI[2] = (float)reader.ReadDouble();
+            npc.localAI[3] = (float)reader.ReadDouble();
+            PassiveDialogueState = (float)reader.ReadDouble();
         }
 
-        private void chat(string text, bool dramatic = false)
+        private void chat(string text, bool dramatic = false, bool sad = false)
         {
             if (Main.netMode == NetmodeID.SinglePlayer)
-                Main.NewText($"<Ethos of Terraria> {text}" + (dramatic ? "!!" : ""), Color.LimeGreen * (dramatic ? 1.85f : 0.95f));
+                Main.NewText($"<Ethos of Terraria> {text}" + (dramatic ? "!!" : ""), (sad ? Color.DarkGray : Color.LimeGreen * (dramatic ? 1.85f : 0.95f)));
             else
-                NetMessage.BroadcastChatMessage(NetworkText.FromLiteral($"<Ethos of Terraria> {text}"), Color.LimeGreen * (dramatic ? 1.85f : 0.95f));
+                NetMessage.BroadcastChatMessage(NetworkText.FromLiteral($"<Ethos of Terraria> {text}"), (sad ? Color.DarkGray : Color.LimeGreen * (dramatic ? 1.85f : 0.95f)));
+        }
+
+        private void copypasta(string[] text, int expectedSaid)
+        {
+            if (expectedSaid == saidCopypasta) return;
+
+            try
+            {
+                if (Main.GameUpdateCount % 10 == 0)
+                {
+                    CombatText.NewText(new Rectangle((int)npc.position.X + Main.rand.Next(-160, 161), (int)npc.position.Y + Main.rand.Next(-160, 161), 28, 24), Color.LimeGreen, text[copypastaCounter], true);
+                    copypastaCounter++;
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                copypastaCounter = 0;
+                saidCopypasta++;
+            }
         }
     }
 }
