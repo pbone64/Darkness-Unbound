dc071612aa042cae8a5b270dc2c58a742a683ba9
Pbone3 <captainfluffypants0@gmail.com>
2020-08-15 09:01:54 -0500

    Depreciated code and removed usings

--- DUPlayer.cs
+++ DUPlayer.cs
@@ -1,15 +1,8 @@
-﻿using DarknessUnbound.Helpers;
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 
 namespace DarknessUnbound
 {
     public class DUPlayer : ModPlayer
     {
-        public override void PreUpdate()
-        {
-            //AnimatedColor.Update();
-        }
     }
 }
