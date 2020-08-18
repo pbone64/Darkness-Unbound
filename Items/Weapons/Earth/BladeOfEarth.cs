45ffd9711b9ea0002114406b58c133f40b8ee85d
Pbone3 <captainfluffypants0@gmail.com>
2020-08-18 18:42:41 -0500

    Ungodly funny sword added

---
+++ Items/Weapons/Earth/BladeOfEarth.cs
@@ -0,0 +1,44 @@
+﻿using DarknessUnbound.Items.Materials.Souls;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items.Weapons.Earth
+{
+    public class BladeOfEarth : DarknessItem
+    {
+        public override void SafeSetDefaults()
+        {
+            item.damage = 75;
+            item.crit = 8;
+            item.melee = true;
+            item.useTime = item.useAnimation = 26;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+            item.rare = ItemRarityID.LightRed;
+            item.scale = 1.45f;
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox) => Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 1, 0, 0, 0, Color.White);
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<SoulOfEarth>(), 20);
+            recipe.AddIngredient(ItemID.AdamantiteSword);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<SoulOfEarth>(), 20);
+            recipe.AddIngredient(ItemID.TitaniumSword);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
