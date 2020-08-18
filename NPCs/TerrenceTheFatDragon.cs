7ac755ac6cd50e23281f09a267f4957b198616a9
Pbone3 <captainfluffypants0@gmail.com>
2020-08-18 10:28:03 -0500

    Test commit

---
+++ NPCs/TerrenceTheFatDragon.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.NPCs
+{
+    public class TerrenceTheFatDragon : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.Size = new Vector2(64, 128);
+            npc.lifeMax = int.MaxValue;
+        }
+    }
+}
