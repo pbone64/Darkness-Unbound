4923a23ca3f495541f70b10ff2edb994243060cb
Blockaroz <lbpk.123fan@gmail.com>
2020-08-17 20:34:34 -0400

    souuuuuuuuuuuls

--- Items/Materials/Souls/SoulOfWater.cs
+++ Items/Materials/Souls/SoulOfWater.cs
@@ -6,12 +6,12 @@ using Terraria.ID;
 
 namespace DarknessUnbound.Items.Materials.Souls
 {
-    public class SoulOfFire : DarknessItem
+    public class SoulOfWater : DarknessItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Soul of Fire");
-            Tooltip.SetDefault("'The essence of raging creatures'");
+            DisplayName.SetDefault("Soul of Water");
+            Tooltip.SetDefault("'The essence of pragmatic creatures'");
 
             Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 4));
             ItemID.Sets.ItemIconPulse[item.type] = true;
@@ -27,7 +27,8 @@ namespace DarknessUnbound.Items.Materials.Souls
             item.rare = ItemRarityID.Orange;
         }
 
-        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI) => Lighting.AddLight(item.position, Color.Orange.ToVector3());
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI) => Lighting.AddLight(item.position, Color.SkyBlue.ToVector3());
+
         public override Color? GetAlpha(Color lightColor)
         {
             return new Color(255, 255, 255, 50);
