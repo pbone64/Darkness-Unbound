46ef56f9edc38a19fba3973b6907ede61055546b
Pbone3 <captainfluffypants0@gmail.com>
2020-08-20 17:16:20 -0500

    More early game throwing weapons

---
+++ Items/Weapons/Throwing/LeadWeight.cs
@@ -0,0 +1,35 @@
+﻿using DarknessUnbound.Projectiles.Weapons.Throwing;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items.Weapons.Throwing
+{
+    public class LeadWeight : DarknessItem
+    {
+        public override void SafeSetDefaults()
+        {
+            item.damage = 16;
+            item.thrown = true;
+            item.useTime = item.useAnimation = 16;
+            item.knockBack = 0.5f;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+            item.shoot = ModContent.ProjectileType<LeadWeightPro>();
+            item.shootSpeed = 6f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.LeadBar, 2);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this, 55);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/Weapons/Throwing/LeadWeightPro.cs
@@ -0,0 +1,23 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Projectiles.Weapons.Throwing
+{
+    public class LeadWeightPro : ModProjectile
+    {
+        public override string Texture => "DarknessUnbound/Items/Weapons/Throwing/LeadWeight";
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(24);
+            aiType = 24;
+        }
+
+        public override bool PreAI()
+        {
+            if (projectile.velocity.Y < 0) projectile.velocity.Y -= (projectile.velocity.Y / 10f);
+            projectile.velocity.X -= (projectile.velocity.X / 90f);
+            return true;
+        }
+    }
+}
