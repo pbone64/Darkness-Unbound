f2bcd9aed14e752d12274ba4a1c00afd11b516e5
Pbone3 <captainfluffypants0@gmail.com>
2020-08-20 20:12:15 -0500

    All the early game projectiles

---
+++ Items/Weapons/Throwing/GoldJavelin.cs
@@ -0,0 +1,35 @@
+﻿using DarknessUnbound.Projectiles.Weapons.Throwing;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items.Weapons.Throwing
+{
+    public class GoldJavelin : DarknessItem
+    {
+        public override void SafeSetDefaults()
+        {
+            item.damage = 20;
+            item.thrown = true;
+            item.useTime = item.useAnimation = 15;
+            item.knockBack = 4f;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+            item.shoot = ModContent.ProjectileType<GoldJavelinPro>();
+            item.shootSpeed = 12f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.GoldBar, 2);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this, 66);
+            recipe.AddRecipe();
+        }
+    }
+}
