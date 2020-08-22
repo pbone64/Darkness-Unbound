7ca298349cf67f24c9c323e9421488311f26e70b
Pbone3 <captainfluffypants0@gmail.com>
2020-08-21 22:47:25 -0500

    Magnade

---
+++ Items/Weapons/Throwing/Unconsumable/MoltenGrenade.cs
@@ -0,0 +1,36 @@
+﻿using DarknessUnbound.Projectiles.Weapons.Throwing;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items.Weapons.Throwing.Unconsumable
+{
+    public class MoltenGrenade : DarknessItem
+    {
+        public override void SetStaticDefaults() => DisplayName.SetDefault("Magnade");
+
+        public override void SafeSetDefaults()
+        {
+            item.damage = 44;
+            item.thrown = true;
+            item.useTime = item.useAnimation = 12;
+            item.knockBack = 10f;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+            item.shoot = ModContent.ProjectileType<MoltenGrenadePro>();
+            item.shootSpeed = 13f;
+            item.rare = ItemRarityID.Orange;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HellstoneBar, 20);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/Weapons/Throwing/MoltenGrenadePro.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Projectiles.Weapons.Throwing
+{
+    public class MoltenGrenadePro : ModProjectile
+    {
+        public override string Texture => "DarknessUnbound/Items/Weapons/Throwing/Unconsumable/MoltenGrenade";
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.Grenade);
+            projectile.thrown = true;
+            aiType = ProjectileID.Grenade;
+        }
+
+        public override bool PreAI()
+        {
+            projectile.rotation *= 0.95f;
+            return true;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 35; i++)
+            {
+                Dust d = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 127, Main.rand.Next(-8, 9), Main.rand.Next(-8, 9));
+                d.scale = 1.1f;
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                Dust d = Dust.NewDustDirect(projectile.position + Vector2.One * 50, projectile.width - 100, projectile.height - 100, DustID.Smoke);
+                d.scale = 2f;
+                d.noGravity = true;
+                d.velocity *= 1.5f;
+            }
+            Main.PlaySound(SoundID.Item, projectile.Center, 14);
+        }
+    }
+}
