3ee20a5441ebc0fb8b17154b217d5cdfe727b5fb
Pbone3 <captainfluffypants0@gmail.com>
2020-08-22 21:36:07 -0500

    Meteorite shuriken

---
+++ Items/Weapons/Throwing/Unconsumable/MeteorShuriken.cs
@@ -0,0 +1,34 @@
+﻿using DarknessUnbound.Projectiles.Weapons.Throwing;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items.Weapons.Throwing.Unconsumable
+{
+    public class MeteorShuriken : DarknessItem
+    {
+        public override void SafeSetDefaults()
+        {
+            item.damage = 21;
+            item.thrown = true;
+            item.useTime = item.useAnimation = 17;
+            item.knockBack = 1f;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+            item.shoot = ModContent.ProjectileType<MeteorShurikenPro>();
+            item.shootSpeed = 12.5f;
+            item.rare = ItemRarityID.Blue;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.MeteoriteBar, 20);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/Weapons/Throwing/MeteorShurikenPro.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Projectiles.Weapons.Throwing
+{
+    public class MeteorShurikenPro : ModProjectile
+    {
+        public override string Texture => "DarknessUnbound/Items/Weapons/Throwing/Unconsumable/MeteorShuriken";
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.Shuriken);
+            projectile.penetrate = 3;
+            aiType = ProjectileID.Shuriken;
+        }
+
+        public override bool PreAI()
+        {
+            projectile.ai[0] -= 0.4f;
+            return true;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) => target.AddBuff(BuffID.OnFire, 240);
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            projectile.penetrate--;
+            if (projectile.penetrate <= 0)
+                projectile.Kill();
+            else
+            {
+                Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+                Main.PlaySound(SoundID.Item10, projectile.position);
+                if (projectile.velocity.X != oldVelocity.X)
+                {
+                    projectile.velocity.X = -oldVelocity.X;
+                }
+                if (projectile.velocity.Y != oldVelocity.Y)
+                {
+                    projectile.velocity.Y = -oldVelocity.Y;
+                }
+            }
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Dust.NewDust(projectile.Center, projectile.width, projectile.height, Main.rand.NextBool(2) ? DustID.t_Meteor : DustID.Fire, 0, 0, 0, default, 0.8f);
+            }
+            Main.PlaySound(SoundID.Dig, projectile.Center);
+        }
+    }
+}
