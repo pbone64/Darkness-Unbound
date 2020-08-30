de9e17a0269a4c7cfc5f3942e8716c1a4c934839
Blockaroz <lbpk.123fan@gmail.com>
2020-08-30 00:21:46 -0400

    HES HERE

---
+++ Items/FloatingDummyItem.cs
@@ -0,0 +1,42 @@
+﻿using DarknessUnbound.NPCs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.Items
+{
+    public class FloatingDummyItem : DarknessItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Floating Target Dummy");
+            Tooltip.SetDefault("Creates a puppet in midair" +
+                "\nRight click to remove a puppet");
+        }
+        //NPC.NewNPC((int)Main.MouseWorld.X, (int)Main.MouseWorld.Y + 20, (ushort)ModContent.NPCType<FloatingDummy>(), 0);
+        public override void SafeSetDefaults()
+        {
+            item.useTime = item.useAnimation = 5;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.noUseGraphic = true;
+            item.value = Item.buyPrice(0, 0, 90, 0);
+            item.autoReuse = true;
+        }
+        public override bool UseItem(Player player)
+        {
+            if (player.altFunctionUse == 0)
+                NPC.NewNPC((int)Main.MouseWorld.X, (int)Main.MouseWorld.Y + 2, (ushort)ModContent.NPCType<FloatingDummy>(), 0);
+            return true;
+        }
+        public override bool AltFunctionUse(Player player) => true;
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.TargetDummy);
+            recipe.AddIngredient(ItemID.WhiteString, 2);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ NPCs/FloatingDummy.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessUnbound.NPCs
+{
+    public class FloatingDummy : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Puppet");
+            NPCID.Sets.ExcludedFromDeathTally[npc.type] = true;
+            NPCID.Sets.TeleportationImmune[npc.type] = true;
+        }
+        public override bool? DrawHealthBar(byte hbPosition, ref float scale, ref Vector2 position) => false;
+        public override void SetDefaults()
+        {
+            Player player = Main.player[Main.myPlayer];
+            npc.width = 42;
+            npc.height = 38;
+            npc.aiStyle = 0;
+            npc.lifeMax = 100000;
+            npc.HitSound = SoundID.NPCHit15;
+            npc.spriteDirection = -player.direction;
+            npc.defense = 1;
+            npc.damage = 0;
+            npc.knockBackResist = 0f;
+            npc.noGravity = true;
+            npc.chaseable = true;
+            npc.lifeRegen = 90000;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Main.myPlayer];
+            if (npc.life < npc.lifeMax)
+                npc.life += npc.lifeMax;
+
+            float length = Vector2.Distance(Main.MouseWorld, npc.Center);
+
+            if (length < 33 && player.altFunctionUse == 2)
+                npc.active = false;
+
+            if (!npc.active)
+            {
+                Main.PlaySound(SoundID.NPCHit16, npc.Center);
+                for (int i = 0; i < 41; i++)
+                {
+                    Vector2 spin = new Vector2(0, 2).RotatedBy(((MathHelper.Pi * 2f) / 40) * i);
+                    Dust dust = Dust.NewDustPerfect(npc.Center, 204, spin, 0, Color.White, 0.75f);
+                }
+            }
+        }
+        public override bool CheckActive() => false;
+    }
+}
